ffffa46c805a16c722fc9bb6686114ee60b1789f
vrajpatel089 <vrajkpatel882007@gmail.com>
2025-08-22 11:08:00 +0530

    Add files via upload

---
+++ Division.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char * argv[])
+{
+    int a,b,c;
+    printf("Enter two numbers");
+    scanf("%d %d",&a,&b);
+    c = a/b;
+    printf("%d/%d=%d\n",a,b,c);
+    return 0;
+}
+
---
+++ hrs to minutes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char * argv[])
+{
+    int a,60,c;
+    printf("Enter Hours");
+    scanf("%d %d",&a,60);
+    c = a*60;
+    printf("%d/%d=%d\n",a,60,c);
+    return 0;
+}
