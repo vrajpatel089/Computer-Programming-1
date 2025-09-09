dc327a3f242c962a7e4d315e37c7122424368d78
vrajpatel089 <vrajkpatel882007@gmail.com>
2025-09-09 09:19:56 +0530

    Update sum of digits.c

--- sum of digits.c
+++ sum of digits.c
@@ -6,11 +6,12 @@ int main()
     scanf("%d",&b);
     while(b>0)
     {
-        a=b%10;
-        c=c+a;
-        b=b/10;
-    }
+        a=b%10; //to get last digit of number
+        c=c+a; //to add the remainder to 0
+        b=b/10; //to remove the last digit of the number 
+    } //this wil get the remainder add it to zero then remove the last digit and then again repeat the same process until th number becomes zero
     printf("Sum of digits is %d\n",c);
     return 0;
 }
+
 
