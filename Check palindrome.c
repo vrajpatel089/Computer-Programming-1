7e82eff6550c2a894c097287ba10a95d548e3ad2
vrajpatel097 <vrajkpatel882007@gmail.com>
2025-09-09 09:31:48 +0530

    Update Check palindrome.c

--- Check palindrome.c
+++ Check palindrome.c
@@ -4,11 +4,12 @@ int main()
     int n, actual, reversed = 0, remainder;
     printf("Enter a number: ");
     scanf("%d", &n);
-    actual = n;
+    actual = n; // here we are storing the value of original number as it is going to be changed in the loop after running
     for (;n > 0; n = n / 10)
     {
         remainder = n % 10;
-        reversed = reversed * 10 + remainder;
+        reversed = reversed * 10 + remainder; //here after 1st remainder the value of reversed will be equal to the remainder
+         // and in the next iteration it will be equal to the last 2 digits of the number reversed
     }
 
     if (actual == reversed)
@@ -18,3 +19,4 @@ int main()
 
     return 0;
 }
+
