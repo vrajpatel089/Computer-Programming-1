6eb38e61589610cd43311b3c3a329cab0263a93d
vrajpatel097 <vrajkpatel882007@gmail.com>
2025-09-14 23:21:58 +0530

    Add files via upload

--- Bubble sort Decending.c
+++ Bubble sort Decending.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
 void main()
 {
-    int arr[]={5,3,2,1,4},a,b,c,d ,n;
+    int arr[5],a,b,c,d,n,i;
     n=5;
+    for(i=0;i<n;i++)
+    {
+        printf("Enter number %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
     printf("Original Array: ");
     for(a=0;a<n;a++)
     {
         printf("%d\t",arr[a]);
     }
     printf("\n");
-    for(a=0;a<n-1;a++)
+    for(a=0;a<n;a++)
     {
-        for(b=0;b<n-a-1;b++)
+        for(b=0;b<n-a;b++)
         {
             if(arr[b]<arr[b+1])
             {c=arr[b];
